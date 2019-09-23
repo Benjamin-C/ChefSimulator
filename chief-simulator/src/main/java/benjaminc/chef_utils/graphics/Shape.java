73dfab49a55c65f3be4bc402c4c180aa899baddb
Benjamin-C <bencrall@gmail.com>
2019-09-23 11:25:52 -0700

    Finished adding triangle

--- chief-simulator/src/main/java/benjaminc/chef_utils/graphics/Shape.java
+++ chief-simulator/src/main/java/benjaminc/chef_utils/graphics/Shape.java
@@ -2,34 +2,46 @@ package benjaminc.chef_utils.graphics;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
 import java.util.UUID;
 
 public class Shape {
 
 	// Instance Methods
-	private float x = 5;
-	private float y = 5;
-	private float w = 10;
-	private float h = 10;
-	private int r = 255;
-	private int g = 0;
-	private int b = 0;
+	protected float x = 5;
+	protected float y = 5;
+	protected float w = 10;
+	protected float h = 10;
+	protected int r = 255;
+	protected int g = 0;
+	protected int b = 0;
 	
-	private ShapeType shp;
+	protected int xs = 0;
+	protected int ys = 0;
+	protected int ws = 0;
+	protected int hs = 0;
+	protected Polygon triangle;
 	
-	private UUID id;
+	protected int owi = 0;
+	protected int ohe = 0;
+	protected int oox = 0;
+	protected int ooy = 0;
+	protected Graphics ogr = null;
+	
+	protected ShapeType shp;
+	
+	protected boolean recalc = false;
+	
+	protected UUID id;
 	
 	public Shape(ShapeType shp, float x, float y, float w, float h, int r, int g, int b) {
 		super();
 		this.shp = shp;
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-		this.r = r;
-		this.g = g;
-		this.b = b;
+		triangle = new Polygon();
+		setSize(x, y, w, h);
+		setColor(r, g, b);
 		id = UUID.randomUUID();
+		
 	}
 
 	public void draw(int wi, int he, Graphics gr) {
@@ -37,16 +49,46 @@ public class Shape {
 	}
 	public void draw(int wi, int he, int ox, int oy, Graphics gr) {
 		gr.setColor(new Color(r, g, b));
-		int xs = (int) (x*wi) + ox;
-		int ys = (int) (y*he) + oy;
-		int ws = (int) (w*wi);
-		int hs = (int) (h*he);
+		
+		if(!(wi == owi && he == ohe && ox == oox && oy == ooy && gr == ogr) || recalc) {
+			xs = (int) (x*wi) + ox;
+			ys = (int) (y*he) + oy;
+			ws = (int) (w*wi);
+			hs = (int) (h*he);
+			
+			switch(shp) {
+			case EMPTY_TRIANGLE_H: // or next line
+			case SOLID_TRIANGLE_H: {
+				triangle.reset();
+				triangle.addPoint(xs, ys);
+				triangle.addPoint(xs, ys + hs);
+				triangle.addPoint(xs + ws, ys + (hs/2));
+			} break;
+
+			case EMPTY_TRIANGLE_V: // or next line
+			case SOLID_TRIANGLE_V: {
+				triangle.reset();
+				triangle.addPoint(xs, ys);
+				triangle.addPoint(xs + ws, ys);
+				triangle.addPoint(xs + (ws/2), ys + hs);
+			} break;
+			default: break;
+			}
+			if(shp == ShapeType.EMPTY_TRIANGLE_V || shp == ShapeType.SOLID_TRIANGLE_V) {
+				
+			}
+		}
+		
 		switch(shp) {
 		case EMPTY_ECLIPSE: gr.drawOval(xs, ys, ws, hs); break;
 		case EMPTY_RECTANGLE:gr.drawRect(xs, ys, ws, hs);  break;
-		case LINE: gr.drawLine(xs, ys, ws, hs); break;
+		case LINE: gr.drawLine(xs, ys, xs+ws, ys+hs); break;
 		case SOLID_ELIPSE: gr.fillOval(xs, ys, ws, hs);  break;
 		case SOLID_RECTANTLE: gr.fillRect(xs, ys, ws, hs); break;
+		case SOLID_TRIANGLE_V: gr.fillPolygon(triangle); break;
+		case EMPTY_TRIANGLE_V: gr.drawPolygon(triangle); break;
+		case SOLID_TRIANGLE_H: gr.fillPolygon(triangle); break;
+		case EMPTY_TRIANGLE_H: gr.drawPolygon(triangle); break;
 		}
 	}
 	
@@ -55,11 +97,13 @@ public class Shape {
 		this.y = y;
 		this.w = w;
 		this.h = h;
+		recalc = true;
 	}
 	public void setColor(int r, int g, int b) {
 		this.r = r;
 		this.g = g;
 		this.b = b;
+		recalc = true;
 	}
 	
 	public float getX() {
